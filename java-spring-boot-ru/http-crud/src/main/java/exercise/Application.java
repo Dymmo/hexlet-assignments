a91c7ecfecbe39fbb0c3f73db0adc4669748d248
@hexlet/cli <support@hexlet.io>
2025-03-05 10:47:40 +0300

    submit java-spring-boot-ru/http-crud

--- java-spring-boot-ru/http-crud/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/http-crud/src/main/java/exercise/Application.java
@@ -1,27 +1,18 @@
 package exercise;
 
-import java.util.List;
-import java.util.Optional;
-
+import exercise.model.Post;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import exercise.model.Post;
-import org.yaml.snakeyaml.events.Event;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
 public class Application {
     // Хранилище добавленных постов
-    private List<Post> posts = Data.getPosts();
+    private final List<Post> posts = Data.getPosts();
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
