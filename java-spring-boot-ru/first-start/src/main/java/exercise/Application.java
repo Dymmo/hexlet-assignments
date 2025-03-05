840643581cbd2a01513e1d95ce4e66d43bfa5428
@hexlet/cli <support@hexlet.io>
2025-03-05 08:42:10 +0300

    submit java-spring-boot-ru/first-start

---
+++ java-spring-boot-ru/first-start/src/main/java/exercise/Application.java
@@ -0,0 +1,25 @@
+package exercise;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// BEGIN
+@RestController
+@SpringBootApplication
+public class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    @GetMapping("/about")
+    public String about() {
+        log.atInfo().log("/about is called");
+        return "Welcome to Hexlet!";
+    }
+}
+// END
