8e07de130d75ffdab1ea0113c61227ed6f55c8c4
@hexlet/cli <support@hexlet.io>
2025-03-10 10:08:06 +0300

    submit java-spring-boot-ru/dto-to-entity

---
+++ java-spring-boot-ru/dto-to-entity/src/main/java/exercise/dto/ContactDTO.java
@@ -0,0 +1,18 @@
+package exercise.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+public class ContactDTO {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+}
